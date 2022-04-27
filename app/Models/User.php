f562a93a8c6584173c0d34425a89bf44d948b480
ilhamhakim1205 <m.ilhamhakim@gmail.com>
2022-04-27 13:24:52 +0700

    crud:2

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function todos(){
+        return $this->hasMany(Todo::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
